a1d71248bb9c61c7ac0efe418775904064eda6bd
matthieuthuetdev <mthuet.pro@gmail.com>
2026-09-04 09:32:32 +0200

    display sign in errors

--- TestStrategy/src/Form/SignInType.php
+++ TestStrategy/src/Form/SignInType.php
@@ -7,6 +7,8 @@ use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Email;
+use Symfony\Component\Validator\Constraints\NotBlank;
 
 class SignInType extends AbstractType
 {
@@ -16,6 +18,11 @@ class SignInType extends AbstractType
             ->add('email', EmailType::class, [
                 'label' => 'Adresse e-mail',
                 'mapped' => false,
+                'error_bubbling' => true,
+                'constraints' => [
+                    new NotBlank(message: 'Veuillez renseigner votre adresse e-mail.'),
+                    new Email(message: 'Veuillez renseigner une adresse e-mail valide.'),
+                ],
                 'attr' => [
                     'autocomplete' => 'email',
                 ],
@@ -23,6 +30,10 @@ class SignInType extends AbstractType
             ->add('password', PasswordType::class, [
                 'label' => 'Mot de passe',
                 'mapped' => false,
+                'error_bubbling' => true,
+                'constraints' => [
+                    new NotBlank(message: 'Veuillez renseigner votre mot de passe.'),
+                ],
                 'attr' => [
                     'autocomplete' => 'current-password',
                 ],
